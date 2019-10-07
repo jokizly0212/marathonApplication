b6235e712ee3a753dc14a4ceb8514116f7a02c66
Viet Tran <tranhuyviet@gmail.com>
2019-10-07 22:28:19 +0300

    Request event detail

--- app/src/main/java/fi/metropolia/marathon/view/RequestListAdapter.kt
+++ app/src/main/java/fi/metropolia/marathon/view/RequestListAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import fi.metropolia.marathon.R
 import fi.metropolia.marathon.databinding.RequestItemBinding
@@ -35,6 +36,9 @@ class RequestListAdapter(private val requestList: ArrayList<Request>):
     }
 
     override fun onRequestClick(requestId: Int, v: View) {
+        val action = RequestsFragmentDirections.actionRequestsFragmentToRequestDetail()
+        action.requestId = requestId
+        Navigation.findNavController(v).navigate(action)
     }
 
     class RequestViewHolder(var view: RequestItemBinding): RecyclerView.ViewHolder(view.root)
